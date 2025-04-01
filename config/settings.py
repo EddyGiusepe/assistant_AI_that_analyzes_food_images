7ea39935f50ae5233e4d597b08c8c0f0a5b3dcbc
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-04-01 20:16:37 -0300

    Usando a API do Groq

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
