9b29b3b9acc4bb276b49f9ea0e1a607914a75008
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-04-01 23:57:52 -0300

    Usando cores ANSI no terminal

---
+++ utils/ansi_color_constants.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+"""
+# ANSI codes for colors:
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+MAGENTA = "\033[95m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
